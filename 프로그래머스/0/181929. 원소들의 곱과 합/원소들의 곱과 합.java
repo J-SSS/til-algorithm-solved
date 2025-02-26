4c0c84686b41a5ed3e7feac35976f6f95da3ea50
Dongil Jang <dongr5102@naver.com>
2025-02-26 10:03:29 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.09 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int multiple = 1;
+        int sum = 0;
+        for(int i : num_list){
+            multiple *= i;
+            sum += i;
+        }
+    
+        return multiple < Math.pow(sum,2) ? 1 : 0;
+    }
+}
