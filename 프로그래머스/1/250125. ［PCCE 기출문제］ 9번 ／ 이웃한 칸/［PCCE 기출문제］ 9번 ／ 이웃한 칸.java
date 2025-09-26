9aebd1b9d2b6f96c64e704019cf4a383e7d37fc1
Dongil Jang <dongr5102@naver.com>
2025-09-26 10:06:21 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.03 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] board, int h, int w) {
+        int answer = 0;
+        
+        int[] dh = {0, 1, -1, 0};
+        int[] dw = {1, 0, 0, -1};
+        
+        String trgt = board[h][w];
+        for(int i = 0; i < 4; i++){
+            int nh = h + dh[i];
+            int nw = w + dw[i];
+
+            if(nh < 0 || nh >= board.length || nw < 0 || nw >= board[0].length){
+                continue;
+            }
+
+            if(board[nh][nw].equals(trgt)){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
